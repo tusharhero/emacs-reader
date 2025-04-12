a01cfc1550dd0c792e06427a6fbccba523cab4cc
Divya Ranjan <divya@subvertising.org>
2025-04-12 19:18:09 +0000

    Move helper functions from render-pdf.c to helpers

--- render/helpers.c
+++ render/helpers.c
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+// Change an Emacs string to a C string
 bool emacs_2_c_str (emacs_env *env, emacs_value value, char **buffer, size_t *size)
 {
   ptrdiff_t buffer_size;
@@ -30,4 +31,89 @@ bool emacs_2_c_str (emacs_env *env, emacs_value value, char **buffer, size_t *si
   assert (buffer_size == old_buffer_size);
   *size = (size_t) (buffer_size - 1);
   return true;
+}
+
+// Create output buffers for the page
+bool create_buffers(fz_context *ctx, fz_buffer **curr_buf, fz_buffer **prev_buf,
+                    fz_buffer **next_buf) {
+  fz_try(ctx) {
+    *curr_buf = fz_new_buffer(ctx, 1024);
+    *prev_buf = fz_new_buffer(ctx, 1024);
+    *next_buf = fz_new_buffer(ctx, 1024);
+  }
+
+  fz_catch(ctx) {
+    fprintf(stderr, "Cannot create buffer: %s\n", fz_caught_message(ctx));
+
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
+// Create Outputs
+bool create_outputs(fz_context *ctx, fz_output **curr_out, fz_output **prev_out,
+                    fz_output **next_out, fz_buffer *curr_buf,
+                    fz_buffer *prev_buf, fz_buffer *next_buf) {
+  fz_try(ctx) {
+    *curr_out = fz_new_output_with_buffer(ctx, curr_buf);
+    *prev_out = fz_new_output_with_buffer(ctx, prev_buf);
+    *next_out = fz_new_output_with_buffer(ctx, next_buf);
+  }
+
+  fz_catch(ctx) {
+    fprintf(stderr, "Cannot create output: %s\n", fz_caught_message(ctx));
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
+// Dropping and closing functions
+void drop_all_buffers(fz_context *ctx, fz_buffer *curr, fz_buffer *prev,
+                      fz_buffer *next) {
+  if (curr)
+    fz_drop_buffer(ctx, curr);
+  if (prev)
+    fz_drop_buffer(ctx, prev);
+  if (next)
+    fz_drop_buffer(ctx, next);
+}
+
+void drop_all_outputs(fz_context *ctx, fz_output *curr, fz_output *prev,
+                      fz_output *next) {
+  if (curr)
+    fz_drop_output(ctx, curr);
+  if (prev)
+    fz_drop_output(ctx, prev);
+  if (next)
+    fz_drop_output(ctx, next);
+}
+
+void drop_all_devices(fz_context *ctx, fz_device *curr, fz_device *prev,
+                      fz_device *next) {
+  if (curr)
+    fz_drop_device(ctx, curr);
+  if (prev)
+    fz_drop_device(ctx, prev);
+  if (next)
+    fz_drop_device(ctx, next);
+}
+
+void close_all_outputs(fz_context *ctx, fz_output *curr, fz_output *prev,
+                       fz_output *next) {
+  if (curr)
+    fz_close_output(ctx, curr);
+  if (prev)
+    fz_close_output(ctx, prev);
+  if (next)
+    fz_close_output(ctx, next);
+}
+
+void close_all_devices(fz_context *ctx, fz_device *curr, fz_device *prev,
+                       fz_device *next) {
+  if (curr)
+    fz_close_device(ctx, curr);
+  if (prev)
+    fz_close_device(ctx, prev);
+  if (next)
+    fz_close_device(ctx, next);
 }
--- render/helpers.h
+++ render/helpers.h
@@ -6,5 +6,17 @@
 #include <emacs-module.h>
 
 bool emacs_2_c_str(emacs_env *env, emacs_value value, char **buffer, size_t *size);
+bool create_buffers(fz_context *ctx, fz_buffer **curr_buf, fz_buffer **prev_buf,
+                    fz_buffer **next_buf);
+void drop_all_buffers(fz_context *ctx, fz_buffer *curr, fz_buffer *prev,
+                      fz_buffer *next);
+void drop_all_outputs(fz_context *ctx, fz_output *curr, fz_output *prev,
+                      fz_output *next);
+void drop_all_devices(fz_context *ctx, fz_device *curr, fz_device *prev,
+                      fz_device *next);
+void close_all_outputs(fz_context *ctx, fz_output *curr, fz_output *prev,
+                       fz_output *next);
+void close_all_devices(fz_context *ctx, fz_device *curr, fz_device *prev,
+                       fz_device *next);
 
 #endif
