c914f9a8ee51b68f454edc9637eb75c3593d8023
tusharhero <tusharhero@sdf.org>
2025-05-24 11:53:51 +0530

    fix document scaling after theme change.

--- reader.el
+++ reader.el
@@ -472,7 +472,8 @@ Keybindings:
   :lighter " Dark"
   (if reader-dark-mode
       (reader-dyn--set-doc-theme "white" "black")
-    (reader-dyn--set-doc-theme "black" "white")))
+    (reader-dyn--set-doc-theme "black" "white"))
+  (reader-doc-scale-page reader-current-doc-scale-value))
 
 ;; see `reader-saveplace' for details.
 ;;;###autoload
