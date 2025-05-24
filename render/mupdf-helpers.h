82b1e6a97f1e4a83bf4d9b63c9bff786582fc022
Divya Ranjan <divya@subvertising.org>
2025-05-24 05:50:26 +0000

    Remove old DocState from mupdf-helpers

--- render/mupdf-helpers.h
+++ render/mupdf-helpers.h
@@ -20,6 +20,7 @@
 #include <mupdf/fitz.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include "render-core.h"
 
 bool create_buffers(fz_context *ctx, fz_buffer **curr_buf, fz_buffer **prev_buf,
                     fz_buffer **next_buf);
@@ -36,27 +37,6 @@ void close_all_outputs(fz_context *ctx, fz_output *curr, fz_output *prev,
                        fz_output *next);
 void close_all_devices(fz_context *ctx, fz_device *curr, fz_device *prev,
                        fz_device *next);
-
-typedef struct {
-  fz_context *ctx;
-  fz_document *doc;
-  char *path;
-  int pagecount;
-  int prev_page_number;
-  int current_page_number;
-  int next_page_number;
-  char *current_svg_data;
-  size_t current_svg_size;
-  char *next_svg_data;
-  size_t next_svg_size;
-  char *prev_svg_data;
-  size_t prev_svg_size;
-  fz_page *current_page;
-  fz_page *next_page;
-  fz_page *prev_page;
-  fz_rect page_bbox;
-  fz_outline *outline;
-} DocState;
 
 int doc_page_width(DocState *state);
 int doc_page_length(DocState *state);
