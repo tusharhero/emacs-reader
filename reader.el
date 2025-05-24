b6396989688b8cf1c8b51a9f1e70836a4386f040
tusharhero <tusharhero@sdf.org>
2025-05-24 11:49:38 +0530

    add dark theme minor mode

--- reader.el
+++ reader.el
@@ -467,6 +467,13 @@ Keybindings:
 
 (add-hook 'reader-mode-hook #'reader-mode-line)
 
+(define-minor-mode reader-dark-mode
+  "Toggle dark-mode for current reader document."
+  :lighter " Dark"
+  (if reader-dark-mode
+      (reader-dyn--set-doc-theme "white" "black")
+    (reader-dyn--set-doc-theme "black" "white")))
+
 ;; see `reader-saveplace' for details.
 ;;;###autoload
 (advice-add 'save-place-find-file-hook :around #'reader--saveplace-find-file)
