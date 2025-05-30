b81d08af119f50ac0032e0dc7df260c9e6212be7
Divya Ranjan <divya@subvertising.org>
2025-05-30 14:00:45 +0000

    Update version to 0.2.7

--- reader.el
+++ reader.el
@@ -5,7 +5,7 @@
 
 ;; Author: Divya Ranjan Pattanaik <divya@subvertising.org>
 ;; Keywords: lisp, files, tools
-;; Version: 0.2.6
+;; Version: 0.2.7
 ;; Package-Requires: ((emacs "26.1"))
 ;; URL: https://codeberg.org/divyaranjan/emacs-reader
 
