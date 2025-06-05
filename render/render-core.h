b9a5a534fc8415f3c1c500053a8098dcd2b2058e
Divya Ranjan <divya@subvertising.org>
2025-06-05 16:33:29 +0000

    Add thread pool stuff and remove async_slide_stuff
    Indentation

--- render/render-core.h
+++ render/render-core.h
@@ -21,6 +21,8 @@
 #include <mupdf/fitz.h>
 #include <pthread.h>
 
+#define MAX_POOL_SIZE 8
+#define MAX_QUEUE_SIZE 256
 #define MAX_CACHE_SIZE 11
 #define MAX_CACHE_WINDOW (MAX_CACHE_SIZE / 2)
 
@@ -65,28 +67,31 @@ typedef struct {
   CachedPage *cp;
 } RenderThreadArgs;
 
+typedef struct {
+  void *(*func)(void *);
+  void *arg;
+} ThreadJob;
+
 void *render_page_thread(void *arg);
-bool async_render(DocState *state, CachedPage *cp);
+void async_render(DocState *state, CachedPage *cp);
 void build_cache_window(DocState *state, int n);
 bool slide_cache_window_forward(DocState *state);
-void *async_slide_forward(void *args);
 bool slide_cache_window_backward(DocState *state);
-void *async_slide_backward(void *args);
 
 // Emacs module functions
 emacs_value emacs_load_doc(emacs_env *env, ptrdiff_t nargs, emacs_value *args,
-			   void *data);
+                           void *data);
 emacs_value emacs_next_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args,
-			    void *data);
+                            void *data);
 emacs_value emacs_prev_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args,
-			    void *data);
+                            void *data);
 emacs_value emacs_first_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args,
-			     void *data);
+                             void *data);
 emacs_value emacs_last_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args,
-			    void *data);
+                            void *data);
 emacs_value emacs_goto_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args,
-			    void *data);
+                            void *data);
 emacs_value emacs_doc_scale_page(emacs_env *env, ptrdiff_t nargs,
-				 emacs_value *args, void *data);
+                                 emacs_value *args, void *data);
 
 #endif // RENDER_CORE_H
