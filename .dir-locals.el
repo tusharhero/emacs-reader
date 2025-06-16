e5ba25cface0162a6b769e323330339b978572ae
tusharhero <tusharhero@sdf.org>
2025-06-16 10:48:58 +0530

    remove elisp settings from .dir-locals.el
    * it is better to stick to defaults for elisp.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,8 +4,5 @@
             (tab-width . 8)              ; Tab width (in case tabs are viewed)
             (c-default-style . "gnu")    ; Use GNU C style
             (fill-column . 80)))
- (emacs-lisp-mode . ((indent-tabs-mode . nil)
-                     (lisp-indent-offset . 2)
-                     (fill-column . 80)))
  (nil . ((require-final-newline . t)
          (show-trailing-whitespace . t))))
