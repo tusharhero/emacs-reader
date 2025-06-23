2ec7e5daf1f7de3dfb526ba826de26f77e468c3c
tusharhero <tusharhero@sdf.org>
2025-06-23 20:22:40 +0530

    chore: add all packages in manifest.scm to default.nix too

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+# Nix shell support is added, but most of us do not use Nix. So GNU
+# Guix is preferred if you can choose.
+
+{ pkgs ? import <nixpkgs> { }, }:
 
 pkgs.mkShell {
-  packages = [ pkgs.mupdf-headless ];
+  packages =
+    [ pkgs.emacs pkgs.mupdf-headless pkgs.gnumake pkgs.clang-tools pkgs.gcc ];
 }
