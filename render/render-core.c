8ddd7109417aeab9e60f7fa4312bdbcda6322866
Divya Ranjan <divya@subvertising.org>
2025-07-09 10:52:31 +0000

    refactor: emacs_next_page should incorporate EmacsWinState
    * this is required in order to slide the cache_window of that
    particular window

--- render/render-core.c
+++ render/render-core.c
@@ -571,12 +571,14 @@ emacs_next_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args, void *data)
 	(void)args;
 	(void)data;
 
-	DocState *state = get_doc_state_ptr(env);
+	DocState *doc_state = get_doc_state_ptr(env);
+	EmacsWinState *win_state = get_win_state_ptr(env);
 	emacs_value current_doc_overlay = get_current_doc_overlay(env);
 
-	if (state)
+	if (doc_state && win_state)
 	{
-		if (state->current_page_number == (state->pagecount - 1))
+		if (win_state->current_page_number
+		    == (doc_state->pagecount - 1))
 		{
 			emacs_message(env, "Already last page!");
 			return EMACS_NIL;
@@ -584,9 +586,11 @@ emacs_next_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args, void *data)
 
 		// Get the pointer for the next CachedPage in the window
 		CachedPage *next_cp
-		    = state->cache_window[state->current_window_index + 1];
+		    = win_state
+			  ->cache_window[win_state->current_window_index + 1];
 		DrawThreadArgs *draw_args = malloc(sizeof(DrawThreadArgs));
-		draw_args->state = state;
+		draw_args->doc_state = doc_state;
+		draw_args->win_state = win_state;
 		draw_args->cp = next_cp;
 		submit_job(draw_page_thread, draw_args, &g_thread_pool);
 
@@ -594,9 +598,9 @@ emacs_next_page(emacs_env *env, ptrdiff_t nargs, emacs_value *args, void *data)
 		pthread_mutex_lock(&next_cp->mutex);
 		pthread_cond_wait(&next_cp->cond, &next_cp->mutex);
 		pthread_mutex_unlock(&next_cp->mutex);
-		display_img_to_overlay(env, state, next_cp->img_data,
+		display_img_to_overlay(env, win_state, next_cp->img_data,
 				       next_cp->img_size, current_doc_overlay);
-		slide_cache_window_forward(state);
+		slide_cache_window_forward(doc_state, win_state);
 		return EMACS_T;
 	}
 	else
